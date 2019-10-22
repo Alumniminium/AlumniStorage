4e3eb7a78a056aa8a748216c907789636b916fe6
alumni <alumni@box.alumni.re>
2019-10-23 00:12:33 +0200

    test

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+tmux new-session \; \
+  send-keys 'cd Client && dotnet run' C-m \; \
+  send-keys 'cd Client && dotnet run' C-m \; \
+  split-window -h \; \
+  send-keys '#control+b arrows' C-m \; \
+  send-keys 'cd Server && dotnet run' C-m \; \
+  select-pane -t0 \;
